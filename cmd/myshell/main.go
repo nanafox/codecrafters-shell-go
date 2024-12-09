04cbcc112e255bb804c303c43c3ca84c4cf4ae79
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-12-09 17:20:18 +0000

    feat: Implement a much better `exit` command for the shell

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -5,9 +5,17 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
 
 var lastExitCode int = 0
+
+const EXIT_SUCCESS = 0
+
+const INVALID_ARGUMENT = 1
+
+const ILLEGAL_NUMBER = 2
 
 const COMMAND_NOT_FOUND = 127
 
@@ -38,23 +46,42 @@ func handleCommand(cmd string) int {
 		cmd = cmd[:len(cmd)-1] // string the newline character
 	}
 
+	splitCommand := strings.Split(cmd, " ")
+	majorCommand := splitCommand[0]
+
 	// hard code a few commands to start with
-	if cmd == "exit" {
-		os.Exit(lastExitCode)
+	if majorCommand == "exit" {
+		return handleExit(splitCommand)
 	}
 
 	if cmd == "$?" || cmd == "$status" {
 		fmt.Println(lastExitCode)
-		lastExitCode = 0
-		return 0
+		lastExitCode = EXIT_SUCCESS
+		return lastExitCode
 	}
 
 	// handle empty commands
 	if cmd == "" {
-		return 0
+		return EXIT_SUCCESS
 	}
 
 	fmt.Fprintf(os.Stderr, "%s: command not found\n", cmd)
 	lastExitCode = COMMAND_NOT_FOUND
 	return lastExitCode
+}
+
+// handleExit handles the exit command and exits the shell when the command is
+// called with the correct arguments
+func handleExit(splitCommand []string) (code int) {
+	if len(splitCommand) == 2 {
+		code, err := strconv.Atoi(splitCommand[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "exit: %s: numeric argument required\n", splitCommand[1])
+			lastExitCode = ILLEGAL_NUMBER
+			return lastExitCode
+		}
+		os.Exit(code)
+	}
+	os.Exit(lastExitCode)
+	return
 }
